a5650bfcf2030dbdcfeba2076c69ac6cd8cb65c2
Lenka Lážnovská <159648392+Lenkalaznovska@users.noreply.github.com>
2024-07-27 19:53:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Najdeme ikonu menu a prvek navigace
+  // Find the menu icon and the navigation element
   const menuIcon = document.querySelector(".menu-icon i");
   const nav = document.querySelector("nav");
 
-  // Přidáme událost pro kliknutí na ikonu menu
+  // Add an event listener for clicking the menu icon
   menuIcon.addEventListener("click", () => {
-    // Zkontrolujeme, zda je navigace otevřená
+    // Check if the navigation is open
     if (nav.classList.contains("open")) {
-      // Pokud je otevřená, zavřeme ji a změníme ikonu na "fa-bars"
+      // If it is open, close it and change the icon to "fa-bars"
       nav.classList.remove("open");
       menuIcon.classList.remove("fa-xmark");
       menuIcon.classList.add("fa-bars");
     } else {
-      // Pokud není otevřená, otevřeme ji a změníme ikonu na "fa-xmark"
+      // If it is not open, open it and change the icon to "fa-xmark"
       nav.classList.add("open");
       menuIcon.classList.remove("fa-bars");
       menuIcon.classList.add("fa-xmark");
     }
   });
 
-  // Přidáme událost pro změnu velikosti okna
+  // Add an event listener for window resizing
   window.addEventListener("resize", function () {
-    // Pokud je šířka okna větší než 600px, zavřeme navigaci a změníme ikonu na "fa-bars"
+    // If the window width is greater than 600px, close the navigation and change the icon to "fa-bars"
     if (window.innerWidth > 600) {
       nav.classList.remove("open");
       menuIcon.classList.remove("fa-xmark");
